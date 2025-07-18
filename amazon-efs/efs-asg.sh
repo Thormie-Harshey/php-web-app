e2294fb02c4260a5b5bde64b6710072adb125d06
Thormie Harshey <ashayeadetomiwa77@gmail.com>
2025-07-18 14:49:13 +0100

    updated efs dns name

--- amazon-efs/efs-asg.sh
+++ amazon-efs/efs-asg.sh
@@ -42,7 +42,7 @@ sudo systemctl start mysqld
 sudo systemctl enable mysqld
 
 # environment variable
-EFS_DNS_NAME=fs-0010eb9b6121b2db3.efs.eu-west-2.amazonaws.com
+EFS_DNS_NAME=fs-0921aacf010fdcfc3.efs.us-east-1.amazonaws.com
 
 # mount the efs to the html directory 
 echo "$EFS_DNS_NAME:/ /var/www/html nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 0 0" | sudo tee -a /etc/fstab
